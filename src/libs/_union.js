d7b2bf239fe5e778a2115eef02360201f7fff5b5
mikoloism <mikoloism.github@gmail.com>
2021-06-28 21:20:10 +0430

    update (fix) : _union.js ---------------------------------------------------------------- - replace : `from` to `what` - pretty

--- src/libs/_union.js
+++ src/libs/_union.js
@@ -1,35 +1,40 @@
-import { from, to } from '../utils';
+import { to, what } from '../utils';
 
 const union = (...types) => {
-	return {
-		type: to.enumic(types),
-		match(value) {
-			// TODO : use util-type-check
-			let res = types.find((type) => from.construct(value) === type);
-			if (res) return value;
-			return { value, type: from.construct(value) };
-		},
-		matchSync(value) {
-			return new Promise((resolve, reject) => {
-				let res = types.find((type) => from.construct(value) === type);
-				if (res) resolve(value);
-				reject({ value, type: from.construct(value) });
-			});
-		},
-	};
+  return {
+    type: to.enumic(types),
+    match(value) {
+      // TODO : replace `wath.construct` with `wath.proto`
+      let res = types.find((type) => what.construct(value) === type);
+      if (res) return value;
+      return { value, type: what.construct(value) };
+    },
+    matchSync(value) {
+      return new Promise((resolve, reject) => {
+        let res = types.find((type) => what.construct(value) === type);
+        if (res) resolve(value);
+        reject({ value, type: what.construct(value) });
+      });
+    },
+  };
 };
 
 union.from = function (types) {
-	return union(...types);
+  return union(...types);
 };
 
 export default union;
 
 /**
+ * ```
  * let AgeType = union('string', 'number');
- * AgeType.type							   //=> 'string', 'number'
- * AgeType.match('my-value')			   //=> #true  => 'my-value'
- * AgeType.match(['my', 'other', 'value']) //=> #false => { value: [...], type: 'array' }
- * ? nice work with `type`
+ * AgeType.type
+ * //=> 'string', 'number'
+ * AgeType.match('my-value')
+ * //=> #true  => 'my-value'
+ * AgeType.match(['my', 'other', 'value'])
+ * //=> #false => { value: [...], type: 'array' }
+ * ```
+ * ? nice work with `type`\
  * ! need to work with tuple || interface
  */
